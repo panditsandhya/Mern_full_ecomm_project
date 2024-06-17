9d31b637ecb682300dd265a573175578c9299795
Sandhya110 <starsandhya369@gmail.com>
2024-06-17 14:35:59 +0530

    till dashboard

---
+++ routes/authRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//Register || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//protected route auth
+router.get("user-auth", requireSignIn, (req, res) => {
+res.status(200).send({ ok: true });
+});
+
+
+export default router;
